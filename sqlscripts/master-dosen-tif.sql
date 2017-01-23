a6e76e0552f1b8f1aa4883396834a7c7768dca36
barliant <barliant@gmail.com>
2017-01-23 14:49:13 +0700

    Master DLB

---
+++ sqlscripts/master-dosen-tif.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS dosen (
+  nik CHAR(5) PRIMARY KEY,
+  nama TEXT NOT NULL,
+  prodi CHAR(3) NOT NULL
+);
+
+INSERT INTO indukdosen VALUES ('02435', 'Abdul Rochman', 'TIF');
+INSERT INTO indukdosen VALUES ('01683', 'Agung Sediyono', 'TIF');
+INSERT INTO indukdosen VALUES ('10075', 'Agus Salim', 'SIF');
+INSERT INTO indukdosen VALUES ('02461', 'Ahmad Zuhdi', 'TIF');
+INSERT INTO indukdosen VALUES ('02554', 'Anung B Ariwibowo', 'SIF');
+INSERT INTO indukdosen VALUES ('99324', 'Binti Solihah', 'SIF');
+INSERT INTO indukdosen VALUES ('02128', 'Dedy Sugiarto', 'SIF');
+INSERT INTO indukdosen VALUES ('10173', 'Dian Pratiwi', 'TIF');
+INSERT INTO indukdosen VALUES ('01449', 'Djasli Djamarus', 'TIF');
+INSERT INTO indukdosen VALUES ('02472', 'Gatot Budi Santoso', 'TIF');
+INSERT INTO indukdosen VALUES ('99291', 'Is Mardianto', 'TIF');
+INSERT INTO indukdosen VALUES ('01643', 'M Najih', 'TIF');
+INSERT INTO indukdosen VALUES ('90214', 'Ratna Shofiati', 'TIF');
+INSERT INTO indukdosen VALUES ('01762', 'Syaifuddin', 'SIF');
+INSERT INTO indukdosen VALUES ('99284', 'Syandra Sari', 'SIF');
+INSERT INTO indukdosen VALUES ('99299', 'Teddy Siswanto', 'SIF');
